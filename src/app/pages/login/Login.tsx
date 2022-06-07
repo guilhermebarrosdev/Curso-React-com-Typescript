92d0193f9dcf2c07be90c73c9a0f2df36c23d84a
Guilherme Barros <wingui3@gmail.com>
2022-06-07 14:27:31 -0300

    :sparkles: add login page and form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,10 +1,39 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEntrar = () => {
+    console.log(email, password);
+  };
+
   return (
     <>
-      <h1>Entrar</h1>
-      <Link to="/pagina-inicial">Página Inicial</Link>
+      <form>
+        <label>
+          <span>Email</span>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Senha</span>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <button type="button" onClick={handleEntrar}>
+          Entrar
+        </button>
+      </form>
     </>
   );
 };
