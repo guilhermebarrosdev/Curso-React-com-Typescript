1eb7af675f80f359a559de6a878b1d13d212110e
Guilherme Barros <wingui3@gmail.com>
2022-06-07 16:53:28 -0300

    :sparkles: Usando o useRef com ebjetos simples

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,17 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Dashboard = () => {
+  const counterRef = useRef({ count: 0 });
+
   return (
     <>
       <h1>Dashboard</h1>
+      <p>Contador: {counterRef.current.count}</p>
+      <button onClick={() => counterRef.current.count++}>Incrementar</button>
+      <button onClick={() => console.log(counterRef.current.count++)}>
+        log
+      </button>
       <Link to="/entrar">Entrar</Link>
     </>
   );
