0a60d31ce2df63c8bf00486daad7cff89d5cc4b2
Guilherme Barros <wingui3@gmail.com>
2022-06-07 14:42:31 -0300

    :sparkles: add inputLogin component

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,9 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    if (window.confirm('Você é homem?')) {
+      console.log('Homem');
+    } else {
+      console.log('Mulher');
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log('Email:', email);
+  }, [email]);
+
+  useEffect(() => {
+    console.log('Password:', password);
+  }, [password]);
 
   const handleEntrar = () => {
     console.log(email, password);
